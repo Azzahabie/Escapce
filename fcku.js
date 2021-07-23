75f35f5c321222de8858691cb7beb6c402ac7cd1
azzahabie <azzahabie@gmail.com>
2021-07-23 18:46:31 +0800

    done things

--- fcku.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const btn = document.getElementById('ok');
-    btn.addEventListener('click',()=>{
-    console.log("ok");
-})
-})
--- index.js
+++ index.js
@@ -1,40 +1,75 @@
 window.addEventListener('DOMContentLoaded', () => {
-var canvas = document.getElementById("main-canvas");
-var ctx = canvas.getContext("2d");
-var openModalBtn = document.getElementById("modal1")
+
+    //canvas
+    const canvas = document.getElementById("main-canvas");
+    const ctx = canvas.getContext("2d");
+
+    //modal button
+    const canvasBtn = document.getElementById('openCanvas')
+    const openModalBtn = document.getElementById("openModal")
+    const modal = document.getElementById("modal")
+    const closeModalBtn = document.getElementById("closeModal")
 
 
-openModalBtn.onclick = function() {
-    modal.style.display = "block";
-    console.log("ok");
-  }
-function testFunc() {
-    window.alert("func ok")
-}
-function openModal() {
-    console.log("opneing");
-    openModalBtn.style.display = "block";
-}
+    //listeners
+    closeModalBtn.addEventListener('click', () => {
+        closeModal()
+    })
+    canvasBtn.addEventListener('click', () => {
+        openCanvas()
+    })
 
-function drawImage() {
-    img = new Image();
-    img.src = './room.png'
-    img.onload = () => {
-        ctx.drawImage(img, 0, 0)
+    openModalBtn.addEventListener('click', () => {
+        openModal()
+    })
+
+    canvas.addEventListener('click', ((event) => {
+        console.log(event.clientX);
+        console.log(event.clientY);
+    }))
+
+    //functions
+    function openModal() {
+
+        if (modal.style.display === "none") {
+            modal.style.display = "block";
+        } else {
+            modal.style.display = "none";
+        }
     }
-}
+    function closeModal() {
+
+        if (modal.style.display === "none") {
+            modal.style.display = "block";
+        } else {
+            modal.style.display = "none";
+        }
+    }
+    function openCanvas() {
+        if (canvas.hidden == true) {
+            canvas.hidden = false;
+            console.log("h is f");
+        } else {
+           canvas.hidden = true;
+           console.log("h is t");
+        }
+    }
+
+    function drawImage() {
+        img = new Image();
+        img.src = './theImage.png'
+        img.onload = () => {
+            ctx.drawImage(img, 0, 0)
+        }
+    }
+
+    function drawRect(){
+        ctx.begin
+    }
 
 
-canvas.addEventListener('click', ((event) => {
-    console.log(event.clientX);
-    console.log(event.clientY);
-}))
-drawImage()
 
-
-
-
+    //callers
+    drawImage()
+    drawRect()
 })
-
-
-
