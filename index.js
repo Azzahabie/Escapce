7821d294fa2e8e7c997e584d9b65ef0e8898d702
azzahabie <azzahabie@gmail.com>
2021-07-23 21:30:08 +0800

    pushing

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+
+
 window.addEventListener('DOMContentLoaded', () => {
 
     //canvas
     const canvas = document.getElementById("main-canvas");
     const ctx = canvas.getContext("2d");
+
+    //modal content
+    const modalContent = document.getElementById("modal-h2")
 
     //modal button
     const canvasBtn = document.getElementById('openCanvas')
@@ -12,30 +17,38 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
     //listeners
+    openModalBtn.addEventListener('click', () => {
+        alert('opened')
+        // openModal('hi')
+    })
+
     closeModalBtn.addEventListener('click', () => {
         closeModal()
     })
+
     canvasBtn.addEventListener('click', () => {
         openCanvas()
     })
 
-    openModalBtn.addEventListener('click', () => {
-        openModal()
-    })
-
     canvas.addEventListener('click', ((event) => {
-        console.log(event.clientX);
-        console.log(event.clientY);
+        pressWhere(event.clientX,event.clientY)
+        .then((data)=>{
+            console.log(data)
+            openModal(data)
+        })
+        console.log(event.clientX, event.clientY);
+        
     }))
 
     //functions
-    function openModal() {
-
+    function openModal(modal2) {
         if (modal.style.display === "none") {
             modal.style.display = "block";
         } else {
             modal.style.display = "none";
         }
+        modalContent.textContent = modal2
+
     }
     function closeModal() {
 
@@ -63,13 +76,35 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function drawRect(){
-        ctx.begin
-    }
-
-
+    function pressWhere(mousePosX, mousePosY){
+        return new Promise(function(resolve,reject){
+            if ((mousePosX > 904 && mousePosX < 1104) && (mousePosY > 141 && mousePosY < 429)) {
+                resolve("cardBoard")
+            } else if((mousePosX > 1190 && mousePosX < 1233) && (mousePosY > 348 && mousePosY < 430)) {
+                resolve("keypad")
+            } else if((mousePosX > 1129 && mousePosX < 1166) && (mousePosY > 411 && mousePosY < 499)) {
+                resolve("dustbin")
+            } else if((mousePosX > 333 && mousePosX < 392) && (mousePosY > 435 && mousePosY < 559)) {
+                resolve("glass")
+            } else if((mousePosX > 578 && mousePosX < 720) && (mousePosY > 349 && mousePosY < 413)) {
+                resolve("pillow")
+            } else if((mousePosX > 632 && mousePosX < 695) && (mousePosY > 127 && mousePosY < 199)) {
+                resolve("window")
+            } else if((mousePosX > 1013 && mousePosX < 1104) && (mousePosY > 197 && mousePosY < 235)) {
+                resolve("spiderweb")
+            } else if((mousePosX > 705 && mousePosX < 728) && (mousePosY > 197 && mousePosY < 235)) {
+                resolve("lightbulb")
+            } else if((mousePosX > 551 && mousePosX < 693) && (mousePosY > 225 && mousePosY < 319)) {
+                resolve("luggage")
+            } else if((mousePosX > 470 && mousePosX < 520) && (mousePosY > 320 && mousePosY < 520)) {
+                resolve("floormat")
+            } else if((mousePosX > 272 && mousePosX < 380) && (mousePosY > 99 && mousePosY < 289)) {
+                resolve("picture")
+            }
+        })
+    }   
 
     //callers
     drawImage()
-    drawRect()
+
 })
---
+++ logic.js
@@ -0,0 +1,5 @@
+function pressWhere(){
+    console.log("hi");
+}
+
+export default {pressWhere}
